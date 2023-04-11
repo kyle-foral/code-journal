702859e190eb9c4181cb3004cb52d21d42392d5b
Kyle Foral <kyleforal@gmail.com>
2023-04-11 19:07:47 +0000

    fixed a few errors within the code and adjusted css sheets better

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const photoImg = document.getElementById('myUrl');
-photoImg.addEventListener('input', picUrl);
+const $photoImg = document.getElementById('myUrl');
+$photoImg.addEventListener('input', picUrl);
 
-const img = document.querySelector('img');
+const $img = document.querySelector('img');
 
 function picUrl(event) {
-  img.setAttribute('src', event.target.value);
+  $img.setAttribute('src', event.target.value);
 }
 
-const form = document.querySelector('#form-id');
+const $form = document.querySelector('#form-id');
 
-form.addEventListener('submit', subButton);
+$form.addEventListener('submit', subButton);
 
 function subButton(event) {
   event.preventDefault();
@@ -19,11 +19,10 @@ function subButton(event) {
     photo: event.target.elements.myUrl.value,
     notes: event.target.elements.notes.value
   };
-  if (entry.entryId === data.nextEntryId) {
-    data.nextEntryId++;
-  }
-  data.entries.push(entry);
-  img.setAttribute('src', 'images/placeholder-image-square.jpg');
-  form.reset();
+  data.nextEntryId++;
+
+  data.entries.unshift(entry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 
 }
