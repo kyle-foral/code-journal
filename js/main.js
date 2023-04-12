b3675b5eceb405a79b5f8b4ab9770e7de31ffffc
Kyle Foral <kyleforal@gmail.com>
2023-04-12 04:51:58 +0000

    pushing to pull on laptop

--- js/main.js
+++ js/main.js
@@ -42,7 +42,12 @@ function renderEntry(entry) {
   $rest.className = 'column-half';
 
   const $pic = document.createElement('img');
-  $pic.src = data.entries.photo;
+  if (data.entries.photo === 'undefined') {
+    $pic.src = 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*';
+  } else {
+    $pic.src = data.entries.photo;
+  }
+  // console.log(data.entries.photo);
 
   const $ename = document.createElement('p');
   $ename.textContent = data.entries.title;
